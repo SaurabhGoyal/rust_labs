eb3f016c3968978cb154d2427e24dd960b3c2033
Saurabh Goyal <saurabh.2561@gmail.com>
2024-05-04 16:46:46 +0530

    breakdown into functions

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,13 @@ use rand::{self, Rng};
 use std::{cmp::Ordering, io};
 
 fn main() {
+    guessing_game();
+}
+
+fn guessing_game() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     loop {
-        println!("Enter a random number as your guess.");
-        let mut buff = String::new();
-        io::stdin()
-            .read_line(&mut buff)
-            .expect("error in read_line");
-        let buff: u32 = buff
-            .trim()
-            .parse()
-            .expect("Not a number (secret was {secret_number}).");
+        let buff = take_guess();
         match buff.cmp(&secret_number) {
             Ordering::Greater => println!("Your guess is larger."),
             Ordering::Less => println!("Your guess is smaller."),
@@ -22,4 +18,17 @@ fn main() {
             }
         }
     }
+}
+
+fn take_guess() -> u32 {
+    println!("Enter a random number as your guess.");
+    let mut buff = String::new();
+    io::stdin()
+        .read_line(&mut buff)
+        .expect("error in read_line");
+    let buff: u32 = buff
+        .trim()
+        .parse()
+        .expect("Not a number (secret was {secret_number}).");
+    return buff;
 }
