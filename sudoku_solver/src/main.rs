c1d968a049328757acdccb2164e2d30515f36ef8
Saurabh Goyal <saurabh.2561@gmail.com>
2024-05-15 12:47:00 +0530

    fix mutex locking scope

--- sudoku_solver/src/main.rs
+++ sudoku_solver/src/main.rs
@@ -1,15 +1,15 @@
-#![allow(unused)]
+use chrono::Utc;
 use std::{
-    cell,
     io::{self, Write as _},
     num::Wrapping,
+    ops::DerefMut,
     sync::{Arc, Mutex},
-    thread::{self, JoinHandle},
+    thread::{self},
     time::Duration,
 };
 
-const HANDLE_DELAY: u64 = 300;
-const RENDER_DELAY: u64 = 300;
+const HANDLE_DELAY: u64 = 1000;
+const RENDER_DELAY: u64 = 500;
 
 struct Cell {
     val: usize,
@@ -25,7 +25,7 @@ struct Board {
 impl Board {
     fn pprint(&self) -> (String, bool) {
         let dim = self.s_dim * self.s_dim;
-        let line_sep = format!("\n{}\n", vec!["-"; (4 * dim + 1)].join(""));
+        let line_sep = format!("\n{}\n", vec!["-"; 4 * dim + 1].join(""));
         let mut s = String::new();
         let mut complete = true;
         s.push_str(&line_sep);
@@ -35,9 +35,9 @@ impl Board {
             }
             s.push_str(&format!(
                 "{}{: ^3}|{}",
-                if (i % dim == 0) { "|" } else { "" },
+                if i % dim == 0 { "|" } else { "" },
                 cell.val,
-                if (i % dim == dim - 1) { &line_sep } else { "" }
+                if i % dim == dim - 1 { &line_sep } else { "" }
             ));
         }
         for log in &self.logs {
@@ -61,7 +61,6 @@ fn parse(buf: &str) -> Board {
         return cv.parse::<usize>().unwrap();
     };
     let cell_values: Vec<usize> = buf.split(",").map(parse_cell).collect();
-    dbg!(&cell_values);
     let cell_count = cell_values.len();
     let dim = (cell_count as f64).sqrt() as usize;
     let s_dim = (dim as f64).sqrt() as usize;
@@ -73,7 +72,7 @@ fn parse(buf: &str) -> Board {
         .into_iter()
         .map(|cv| Cell {
             val: cv,
-            candidates: (1..=dim).collect(),
+            candidates: (0..=dim).collect(),
         })
         .collect();
     Board {
@@ -88,46 +87,123 @@ fn main() {
         println!("Enter a sudoku, It must be of size nxn and in a comma separated value set for all cells in a single line");
         let mut buf = String::new();
         io::stdin().read_line(&mut buf).unwrap();
+        if buf.is_empty() {
+            break;
+        }
         if buf.ends_with("\n") {
             buf.pop();
         }
         let board = parse(&buf);
+        let s_dim = board.s_dim;
+        let dim = s_dim * s_dim;
         let board_arc_mutex = Arc::new(Mutex::new(board));
         let mut handles = vec![];
-        let board_arc_mutex_clone = Arc::clone(&board_arc_mutex);
-        let s_dim = board_arc_mutex_clone.lock().unwrap().s_dim;
-        let dim = s_dim * s_dim;
         for index in 0..(dim * dim) {
             let board_arc_mutex_clone = Arc::clone(&board_arc_mutex);
             handles.push(thread::spawn(move || {
-                handle_cell(board_arc_mutex_clone, index)
+                handle_cell(board_arc_mutex_clone, s_dim, index)
             }));
         }
+        // for index in 0..dim {
+        //     for num in 1..=dim {
+        //         let board_arc_mutex_clone = Arc::clone(&board_arc_mutex);
+        //         handles.push(thread::spawn(move || {
+        //             handle_number(board_arc_mutex_clone, index, num)
+        //         }));
+        //     }
+        // }
         let board_arc_mutex_clone = Arc::clone(&board_arc_mutex);
         handles.push(thread::spawn(move || render(board_arc_mutex_clone)));
         for handle in handles {
-            handle.join().unwrap();
+            let _ = handle.join();
         }
     }
 }
 
-fn handle_cell(mut board_arc_mutex: Arc<Mutex<Board>>, index: usize) {
+fn handle_cell(board_arc_mutex: Arc<Mutex<Board>>, s_dim: usize, index: usize) {
+    let dim = s_dim * s_dim;
+    let dcells = dependency_cells(s_dim, index);
+    loop {
+        {
+            let mut mutex_guard = match board_arc_mutex.lock() {
+                Ok(mg) => mg,
+                Err(e) => {
+                    println!("Error in getting lock in handle_cell - {index} - {e}");
+                    return;
+                }
+            };
+            let board = mutex_guard.deref_mut();
+            for di in &dcells {
+                let val = board.cells[*di].val;
+                if val > 0 {
+                    board.cells[index].candidates[val] = 0;
+                }
+            }
+            let candidates: Vec<&usize> = board.cells[index]
+                .candidates
+                .iter()
+                .filter(|x| **x > usize::MIN)
+                .collect();
+            if candidates.len() == 1 {
+                board.logs.push(format!(
+                    "[{:#?}] {:?}, {:?} -> {:?}",
+                    Utc::now().to_rfc3339(),
+                    index / dim,
+                    index % dim,
+                    candidates,
+                ));
+                board.cells[index].val = *candidates[0];
+                break;
+            }
+        }
+        thread::sleep(Duration::from_millis(HANDLE_DELAY));
+    }
+}
+
+fn handle_number(board_arc_mutex: Arc<Mutex<Board>>, box_index: usize, number: usize) {
     let s_dim = board_arc_mutex.lock().unwrap().s_dim;
     let dim = s_dim * s_dim;
+    let index = (box_index / s_dim) * s_dim * dim + ((box_index % s_dim) * s_dim);
+    let ci: usize = index / dim;
+    let cj: usize = index % dim;
+    let div_i = ci / s_dim;
+    let div_j = cj / s_dim;
+    let mut candidates: Vec<usize> = vec![];
+    for i in (div_i * s_dim)..((div_i + 1) * s_dim) {
+        for j in (div_j * s_dim)..((div_j + 1) * s_dim) {
+            candidates.push(i * dim + j);
+        }
+    }
     loop {
-        if board_arc_mutex.lock().unwrap().cells[index].val > 0 {
+        if candidates.len() == 1 {
+            board_arc_mutex.lock().unwrap().cells[candidates[0]].val = number;
             break;
         }
-        let candidates = find_candidates(&board_arc_mutex, index);
-        thread::sleep(Duration::from_millis(HANDLE_DELAY));
-        board_arc_mutex.lock().unwrap().logs.push(format!(
-            "{:?}, {:?} -> {:?}",
-            index / dim,
-            index % dim,
-            candidates,
-        ));
-        if candidates.len() == 1 {
-            board_arc_mutex.lock().unwrap().cells[index].val = candidates[0];
+        for i in (div_i * s_dim)..((div_i + 1) * s_dim) {
+            for j in (div_j * s_dim)..((div_j + 1) * s_dim) {
+                if !board_arc_mutex.lock().unwrap().cells[i * dim + j]
+                    .candidates
+                    .contains(&number)
+                {
+                    candidates.remove(i * dim + j);
+                }
+                if number == 7 && box_index == 0 {
+                    println!(
+                        "Cell {:?},{:?} has candidates {:?}",
+                        i,
+                        j,
+                        board_arc_mutex.lock().unwrap().cells[i * dim + j].candidates,
+                    );
+                }
+            }
+        }
+        if number == 7 && box_index == 0 {
+            println!(
+                "Candidates for {:?} in Box-{:?} -> {:?}",
+                number,
+                box_index + 1,
+                candidates
+            );
         }
     }
 }
@@ -159,39 +235,29 @@ fn dependency_cells(s_dim: usize, index: usize) -> Vec<usize> {
     dcells
 }
 
-fn find_candidates(board_arc_mutex: &Arc<Mutex<Board>>, index: usize) -> Vec<usize> {
-    let s_dim = board_arc_mutex.lock().unwrap().s_dim;
-    let dim = s_dim * s_dim;
-    let mut candidates: Vec<usize> = (0..=(s_dim * s_dim)).collect();
-    for di in dependency_cells(s_dim, index) {
-        // println!(
-        //     "{:?},{:?} -> {:?},{:?}",
-        //     index / dim,
-        //     index % dim,
-        //     di / dim,
-        //     di % dim
-        // );
-        let val = board_arc_mutex.lock().unwrap().cells[di].val;
-        if val > 0 {
-            candidates[val] = 0;
-        }
-    }
-    candidates.into_iter().filter(|x| x > &usize::MIN).collect()
-}
-
 fn clear_screen() {
     print!("{}[2J", 27 as char); // ANSI escape code to clear the screen
     print!("{}[1;1H", 27 as char); // ANSI escape code to move the cursor to the top-left corner
     io::stdout().flush().unwrap(); // Flush stdout to ensure screen is cleared immediately
 }
 
-fn render(board: Arc<Mutex<Board>>) {
+fn render(board_arc_mutex: Arc<Mutex<Board>>) {
     loop {
-        let (s, c) = board.lock().unwrap().pprint();
-        println!("{s}");
-        if c {
-            println!("Done!");
-            break;
+        {
+            let mut mutex_guard = match board_arc_mutex.lock() {
+                Ok(mg) => mg,
+                Err(e) => {
+                    println!("Error in getting lock in render - {e}");
+                    return;
+                }
+            };
+            let board = mutex_guard.deref_mut();
+            let (s, c) = board.pprint();
+            println!("{s}");
+            if c {
+                println!("Done!");
+                break;
+            }
         }
         thread::sleep(Duration::from_millis(RENDER_DELAY));
         clear_screen();
