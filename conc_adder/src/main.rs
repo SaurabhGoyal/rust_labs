f5c09356aa30ef200f5846e9564000ae5ef0e2bd
Saurabh Goyal <saurabh.2561@gmail.com>
2024-05-26 12:35:49 +0530

    add async adder

--- conc_adder/src/main.rs
+++ conc_adder/src/main.rs
@@ -1,3 +1,4 @@
+use futures::executor::block_on;
 use rayon::prelude::*;
 use std::{
     sync::{
@@ -91,14 +92,34 @@ fn add_using_rayon(nums: &Vec<Ival>) -> Ival {
     nums.par_iter().sum()
 }
 
+// Not IO bound, this is just for trying the async semantics.
+async fn adder_async(counter_arc_mutex: Arc<Mutex<Ival>>, num: Ival) {
+    *counter_arc_mutex.lock().unwrap() += num;
+}
+
+async fn adder_async_join(counter_arc_mutex: Arc<Mutex<Ival>>, nums: &Vec<Ival>) {
+    for num in nums {
+        let counter_clone = Arc::clone(&counter_arc_mutex);
+        let num_clone = *num;
+        adder_async(counter_clone, num_clone).await;
+    }
+}
+
+fn add_using_async(nums: &Vec<Ival>) -> Ival {
+    let counter_arc_mutex = Arc::new(Mutex::new(0));
+    block_on(adder_async_join(Arc::clone(&counter_arc_mutex), nums));
+    return *counter_arc_mutex.lock().unwrap();
+}
+
 fn main() {
-    let nums: Vec<Ival> = (2..=2000000).into_iter().collect();
+    let nums: Vec<Ival> = (2..=200000000).into_iter().collect();
     time_it!("Rayon", add_using_rayon(&nums));
     // Interestingly the best time is when we are using a single thread in the pool.
-    for ps in (0..=12).map(|p| 2_i32.pow(p)) {
+    for ps in (0..=4).map(|p| 2_i32.pow(p)) {
         time_it!(
             format!("Threads with pool-size of {}", ps),
             add_using_threads(ps as usize, &nums)
         );
     }
+    time_it!("Async", add_using_async(&nums));
 }
