6896f890932e1f7735a42ba952aa449824217d39
Saurabh Goyal <saurabh.2561@gmail.com>
2024-05-28 12:23:28 +0530

    add randomness to ball's direction when starting from bat

--- unbricked/src/main.rs
+++ unbricked/src/main.rs
@@ -1,4 +1,5 @@
 use console::Term;
+use rand::{thread_rng, Rng};
 use std::{
     io::{self, Write as _},
     process::exit,
@@ -7,10 +8,11 @@ use std::{
     time::Duration,
 };
 
-const REFRESH_RATE_MS: usize = 50;
+const REFRESH_RATE_MS: usize = 20;
 const GRID_HEIGHT: i8 = 30;
-const GRID_WIDTH: i8 = 100;
-const BAT_LENGTH: usize = 16;
+const GRID_WIDTH: i8 = 40;
+const BRICK_ROW_COUNT: i8 = 1;
+const BAT_LENGTH: usize = 20;
 
 #[derive(PartialEq, Eq, Clone)]
 enum State {
@@ -52,6 +54,7 @@ struct Bat {
 
 struct Game {
     grid: Vec<Vec<Cell>>,
+    brick_count: u32,
     ball: Ball,
     bat: Bat,
     last_cmd: String,
@@ -62,13 +65,15 @@ impl Game {
         let mut cells: Vec<Vec<Cell>> = Vec::with_capacity(GRID_HEIGHT as usize);
         let mut ball_index: Pair = (-1, -1);
         let mut bat_index: Pair = (-1, -1);
+        let mut brick_count: u32 = 0;
         let bat_j = GRID_WIDTH / 2 - BAT_LENGTH as i8 / 2;
         for i in 0..GRID_HEIGHT {
             let mut row = Vec::with_capacity(GRID_WIDTH as usize);
             for j in 0..GRID_WIDTH {
                 let mut state = State::Empty;
-                if i >= 3 && i <= 5 {
-                    state = State::Brick
+                if i >= 3 && i < 3 + BRICK_ROW_COUNT {
+                    state = State::Brick;
+                    brick_count += 1;
                 } else if i == GRID_HEIGHT - 2 && j == GRID_WIDTH / 2 {
                     state = State::Ball;
                     ball_index = (i, j);
@@ -95,12 +100,16 @@ impl Game {
                 length: BAT_LENGTH,
             },
             last_cmd: String::new(),
+            brick_count,
         }
     }
 
     fn set_ball(&mut self, index: Pair, speed: u8, direction: Pair) {
         let (i, j) = self.ball.index;
         let (ni, nj) = (index.0, index.1);
+        if self.grid[i as usize][j as usize].state == State::Brick {
+            self.brick_count -= 1;
+        }
         self.grid[i as usize][j as usize].state = State::Empty;
         self.grid[ni as usize][nj as usize].state = State::Ball;
         self.ball.index = (ni, nj);
@@ -124,6 +133,10 @@ impl Game {
     }
 
     fn next(&mut self) {
+        if self.brick_count == 0 {
+            self.set_ball((GRID_HEIGHT - 2, GRID_WIDTH / 2), 0, (-1, -1));
+            self.set_bat((GRID_HEIGHT - 1, GRID_WIDTH / 2 - BAT_LENGTH as i8 / 2));
+        }
         if self.ball.speed == 0 {
             return;
         }
@@ -177,7 +190,11 @@ impl Game {
             }
             s.push('\n')
         }
-        s.push_str(&self.last_cmd);
+        if self.brick_count == 0 {
+            s.push_str("You won!!!");
+        } else {
+            s.push_str(&self.last_cmd);
+        }
         s.push('\n');
         s
     }
@@ -207,6 +224,9 @@ fn render(game: Arc<Mutex<Game>>) {
             let mut g = game.lock().unwrap();
             clear_screen();
             println!("{}", g.view());
+            if g.brick_count == 0 {
+                break;
+            }
             g.next();
         }
         sleep(Duration::from_millis(REFRESH_RATE_MS as u64));
@@ -215,23 +235,36 @@ fn render(game: Arc<Mutex<Game>>) {
 
 fn read_input(game: Arc<Mutex<Game>>) -> ! {
     let stdout = Term::buffered_stdout();
+    let mut rng = thread_rng();
     loop {
         {
             if let Ok(character) = stdout.read_char() {
                 let mut g = game.lock().unwrap();
                 g.last_cmd = String::from(format!("User pressed {character}"));
+                let mut dir = (rng.gen_range(-2..=-1), rng.gen_range(-2..=2));
+                if dir.1 == 0 {
+                    dir.1 = 1;
+                }
                 match character {
                     ' ' => {
                         let (ni, nj) = (g.ball.index.0, g.ball.index.1);
-                        g.set_ball((ni, nj), 1, (-1, -1));
+                        g.set_ball((ni, nj), 1, dir);
                     }
                     'a' => {
                         let (ni, nj) = (g.bat.index.0, g.bat.index.1 - 1);
                         g.set_bat((ni, nj));
+                        if g.ball.speed == 0 {
+                            let (ni, nj) = (g.ball.index.0, g.ball.index.1 - 1);
+                            g.set_ball((ni, nj), 0, dir);
+                        }
                     }
                     'd' => {
                         let (ni, nj) = (g.bat.index.0, g.bat.index.1 + 1);
                         g.set_bat((ni, nj));
+                        if g.ball.speed == 0 {
+                            let (ni, nj) = (g.ball.index.0, g.ball.index.1 + 1);
+                            g.set_ball((ni, nj), 0, dir);
+                        }
                     }
                     _ => exit(0),
                 }
