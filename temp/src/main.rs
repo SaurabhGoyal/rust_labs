74057c6c3adce880a1f6dad4b8d00345525611d1
Saurabh Goyal <saurabh.2561@gmail.com>
2024-06-07 12:13:27 +0530

    add local async file walk

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,17 +1,31 @@
-use console::Term;
+use futures::executor::block_on;
+use std::{env, fs, path::Path, rc::Rc};
 
 fn main() {
-    let stdout = Term::buffered_stdout();
+    let args = env::args().collect::<Vec<String>>();
+    let process: ProcessFunc = Rc::new(|p| println!("{p}"));
+    block_on(process_file_path(
+        String::from(&args[1]),
+        Rc::clone(&process),
+    ));
+}
 
-    'game_loop: loop {
-        if let Ok(character) = stdout.read_char() {
-            match character {
-                'w' => println!("Up"),
-                'a' => println!("Left"),
-                's' => println!("Down"),
-                'd' => println!("Right"),
-                _ => break 'game_loop,
-            }
+type ProcessFunc = Rc<dyn Fn(String) + Send + 'static>;
+
+async fn process_file_path(source: String, f: ProcessFunc) {
+    let source_path = Path::new(&source)
+        .canonicalize()
+        .expect("invalid source path");
+    let source_path_str = source_path.as_path().to_str().unwrap().to_string();
+    f(source_path_str);
+    if source_path.is_dir() {
+        for child in fs::read_dir(source_path).unwrap() {
+            let child = child.unwrap();
+            Box::pin(process_file_path(
+                child.path().as_path().to_str().unwrap().to_string(),
+                Rc::clone(&f),
+            ))
+            .await;
         }
     }
 }
