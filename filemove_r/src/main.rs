0c7428d4b03bb08b59d03ccba28d766778925da3
Saurabh Goyal <saurabh.2561@gmail.com>
2024-06-10 17:25:45 +0530

    add async file copier function

--- filemove_r/src/main.rs
+++ filemove_r/src/main.rs
@@ -1,24 +1,33 @@
+use futures::executor::block_on;
 use std::{
     env, fs,
     io::{Read, Write},
     path::Path,
-    sync::mpsc::{channel, Receiver, Sender},
+    sync::{atomic::AtomicUsize, mpsc::{channel, Receiver, Sender}},
     thread,
 };
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let (mover, event_receiver) = Mover::new();
-    let handle = thread::spawn(move || {
+    let transfer_thread = thread::spawn(move || {
         let args = args;
-        mover.transfer(String::from(&args[1]), String::from(&args[2]));
-    });
-    while let Ok(event) = event_receiver.recv() {
-        if event.event_type == EventType::CompletedCopying {
-            println!("{:?}", event);
+        if args.len() > 3 && args[3] == "-a" {
+            println!("<------------- Async ------------->");
+            block_on(mover.transfer_async(String::from(&args[1]), String::from(&args[2])));
+        } else {
+            mover.transfer(String::from(&args[1]), String::from(&args[2]));
         }
-    }
-    handle.join().unwrap();
+    });
+    let event_thread = thread::spawn(move || {
+        while let Ok(event) = event_receiver.recv() {
+            if event.event_type == EventType::CompletedCopying {
+                println!("{:?}", event);
+            }
+        }
+    });
+    transfer_thread.join().unwrap();
+    event_thread.join().unwrap();
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -33,7 +42,17 @@ struct Event {
     event_type: EventType,
 }
 
+struct MoverState {
+    file_count: usize,
+    folder_count: usize,
+    total_bytes: usize,
+    copied_file_count: AtomicUsize,
+    copied_bytes: AtomicUsize,
+}
+
 struct Mover {
+    state: {
+    },
     event_sender: Sender<Event>,
 }
 
@@ -43,38 +62,33 @@ impl Mover {
         (Mover { event_sender }, event_receiver)
     }
 
-    fn transfer(&self, source: String, dest_dir: String) {
+    fn init_path(
+        &self,
+        source: &String,
+        dest_dir: &String,
+    ) -> (std::path::PathBuf, std::path::PathBuf) {
         self.event_sender
             .send(Event {
-                source: source.clone(),
+                source: source.to_string(),
                 event_type: EventType::StartedCopying,
             })
             .unwrap();
         let source_path = Path::new(&source)
             .canonicalize()
             .expect("invalid source path");
-        let dest_dir_path = Path::new(&dest_dir)
+        let dest_path = Path::new(&dest_dir)
             .canonicalize()
-            .expect("invalid dest dir path");
-        let dest_path = dest_dir_path.join(source_path.file_name().unwrap());
-        if source_path.is_dir() {
-            fs::create_dir(dest_path.clone()).expect(
-                "Destination either already exists or does not have the given parent path.",
-            );
-            for child in fs::read_dir(source_path).unwrap() {
-                let child = child.unwrap();
-                self.transfer(
-                    child.path().as_path().to_str().unwrap().to_string(),
-                    dest_path.to_str().unwrap().to_string(),
-                )
-            }
-            return;
-        }
+            .expect("invalid dest dir path")
+            .join(source_path.file_name().unwrap());
+        (source_path, dest_path)
+    }
+
+    fn copy(&self, source_path: &Path, dest_path: &Path) {
         let mut source_file = fs::OpenOptions::new().read(true).open(source_path).unwrap();
         let mut dest_file = fs::OpenOptions::new()
             .create_new(true)
             .write(true)
-            .open(dest_path.clone())
+            .open(dest_path)
             .unwrap();
         // copy(&mut source_file, &mut dest_file).expect("error in copying");
         let mut buf = [0; 1 << 12];
@@ -90,9 +104,46 @@ impl Mover {
         }
         self.event_sender
             .send(Event {
-                source: source.clone(),
+                source: source_path.to_str().unwrap().to_string(),
                 event_type: EventType::CompletedCopying,
             })
             .unwrap();
+    }
+
+    async fn transfer_async(&self, source: String, dest_dir: String) {
+        let (source_path, dest_path) = self.init_path(&source, &dest_dir);
+        if source_path.is_dir() {
+            fs::create_dir(dest_path.clone()).expect(
+                "Destination either already exists or does not have the given parent path.",
+            );
+            for child in fs::read_dir(source_path).unwrap() {
+                let child = child.unwrap();
+                Box::pin(self.transfer_async(
+                    child.path().as_path().to_str().unwrap().to_string(),
+                    dest_path.to_str().unwrap().to_string(),
+                ))
+                .await;
+            }
+            return;
+        }
+        self.copy(&source_path, &dest_path);
+    }
+
+    fn transfer(&self, source: String, dest_dir: String) {
+        let (source_path, dest_path) = self.init_path(&source, &dest_dir);
+        if source_path.is_dir() {
+            fs::create_dir(dest_path.clone()).expect(
+                "Destination either already exists or does not have the given parent path.",
+            );
+            for child in fs::read_dir(source_path).unwrap() {
+                let child = child.unwrap();
+                self.transfer(
+                    child.path().as_path().to_str().unwrap().to_string(),
+                    dest_path.to_str().unwrap().to_string(),
+                );
+            }
+            return;
+        }
+        self.copy(&source_path, &dest_path);
     }
 }
