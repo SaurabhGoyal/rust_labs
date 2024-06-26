8f835b946f84ba85c946ae49f9545c68fc2037c7
Saurabh Goyal <saurabh.2561@gmail.com>
2024-06-26 11:25:27 +0530

    test tcp stream cloning for concurrent reads and writes

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,26 +1,34 @@
 use std::{
+    env,
+    io::{Read, Write},
+    net::TcpStream,
     thread,
-    time::{Duration, Instant},
+    time::Duration,
 };
-use tokio::{self, runtime};
 
 fn main() {
-    let runtime = runtime::Runtime::new().unwrap();
-    runtime.block_on(async {
-        tokio::spawn(async { test("t1").await });
-        tokio::spawn(async { test("t2").await });
-        println!("Tasks spawned!");
-    });
-}
-
-async fn test(msg: &str) {
-    let mut i = 0;
-    loop {
-        println!("{:?} {msg}", Instant::now());
-        thread::sleep(Duration::from_millis(500));
-        i += 1;
-        if i > 10 {
-            break;
+    let args = env::args().collect::<Vec<String>>();
+    let stream = TcpStream::connect((args[1].as_str(), args[2].parse::<u16>().unwrap())).unwrap();
+    let mut stream_tx = stream.try_clone().unwrap();
+    let mut stream_rx = stream.try_clone().unwrap();
+    let handle_tx = thread::spawn(move || {
+        let mut i = 0;
+        loop {
+            let _wb = stream_tx
+                .write(format!("hello - {i}\n").as_bytes())
+                .unwrap();
+            i += 1;
+            thread::sleep(Duration::from_secs(10));
         }
-    }
+    });
+    let handle_rx = thread::spawn(move || loop {
+        let mut buf = [0_u8; 100];
+        let rb = stream_rx.read(&mut buf).unwrap();
+        println!(
+            "Read {rb} bytes - {}",
+            std::str::from_utf8(&buf[..rb]).unwrap()
+        );
+    });
+    handle_tx.join().unwrap();
+    handle_rx.join().unwrap();
 }
